f36f75625a0c3d1d2e041400a33196e00b2ff48b
Aman <amsingh714@gmail.com>
2019-01-23 17:39:54 -0500

    able to GET users

--- index.js
+++ index.js
@@ -57,10 +57,6 @@ function generateToken(user) {
 }
 
 
-
-
-
-
 server.post("/api/login", (req, res) => {
     const credentials = req.body;
 
@@ -77,6 +73,35 @@ server.post("/api/login", (req, res) => {
     })
     .catch(err => res.status(500).json(err));
 })
+
+function lock(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid Token" })
+            } else {
+                req.decodedToken = decodedToken;
+                next()
+            }
+        })
+    } else {
+        res.status(401).json({ message: "No token provided" })
+    }
+}
+
+
+server.get("/api/users", lock, (req, res) => {
+    db("users")
+    .select("id", "username", "department", "password")
+    .then(users => {
+        res.status(200).json({ users })
+    })
+    .catch(err => res.send(err));
+})
+
+
 
 
 

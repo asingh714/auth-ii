c8931a9adb1deaaeea0d15d3d4384b0963029a14
Aman <amsingh714@gmail.com>
2019-04-03 15:46:18 -0400

    able to login and GET users

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,7 +6,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const secrets = require("../api/secrets");
-const Users = require("../database/dbConfig");
+const UsersDB = require("../database/dbConfig");
+const restricted = require("../auth/restricted");
 
 router.post("/register", (req, res) => {
   const user = req.body;
@@ -18,11 +19,11 @@ router.post("/register", (req, res) => {
   } else {
     const hash = bcrypt.hashSync(user.password, 14);
     user.password = hash;
-    Users("users")
+    UsersDB("users")
       .insert(user)
       .then(ids => {
         const id = ids[0];
-        Users("users")
+        UsersDB("users")
           .where({ id })
           .first()
           .then(user => {
@@ -37,6 +38,45 @@ router.post("/register", (req, res) => {
       });
   }
 });
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({
+      error: "Please provide a username and password"
+    });
+  } else {
+    UsersDB("users")
+      .where({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res
+            .status(200)
+            .json({ message: `${user.username} is logged in.`, token });
+        } else {
+          res.status(401).json({ message: "You shall not pass!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while logging in."
+        });
+      });
+  }
+});
+
+router.get("/users", restricted, (req, res) => {
+  UsersDB("users")
+    .select("id", "username", "department")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
 
 function generateToken(user) {
   const payload = {
@@ -53,5 +93,7 @@ function generateToken(user) {
 
   return jwt.sign(payload, secret, options);
 }
+
+
 
 module.exports = router;

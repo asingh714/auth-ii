e12441c20284f3d82e33a3f4fd495f93cd60bb72
Aman <amsingh714@gmail.com>
2019-04-04 16:57:37 -0400

    users are showing

--- client/src/Users/Users.js
+++ client/src/Users/Users.js
@@ -1,18 +1,37 @@
 import React from "react";
 import axios from "axios";
 
+import requiresAuth from "../auth/requiresAuth";
+
 class Users extends React.Component {
   state = {
     users: []
-  }
+  };
 
   componentDidMount() {
+    const endpoint = "http://localhost:5000/api/users";
 
+    const token = localStorage.getItem("token");
+
+    const requestOptions = {
+      headers: {
+        authorization: token
+      }
+    };
+
+    axios
+      .get(endpoint, requestOptions)
+      .then(response => {
+        this.setState({
+          users: response.data
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <>  
+      <>
         <h2>List of Users:</h2>
         {this.state.users.map(user => (
           <div key={user.id}>
@@ -21,7 +40,7 @@ class Users extends React.Component {
           </div>
         ))}
       </>
-    )
+    );
   }
 }
 
